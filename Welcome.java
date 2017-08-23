e8c7846ad39aad44dbc768ace409e1c1e9cbf822
mnalluswamy <mnalluswamy@csc.com>
2017-08-23 14:09:19 +0530

    third commit

--- Welcome.java
+++ Welcome.java
@@ -6,7 +6,7 @@ public class Welcome
 		public static void main(String args[])
 		{
 
-			System.out.println("Welcome to java ");
+			System.out.println("Welcome to java  . Happy Java");
 			System.out.println("i am goint to  print the loop");
 			for(int i=0;i<10;i++)
 			{
