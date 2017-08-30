bd87d9757c84c9fc2dad0fc9a35119b7c72a6430
mnalluswamy <mnalluswamy@csc.com>
2017-08-30 14:30:41 +0530

    adding  welcome1 and  welcome2.java file

---
+++ Welcome2.java
@@ -0,0 +1,19 @@
+public class Welcome1
+{
+
+		public static void main(String args[])
+		{
+
+			System.out.println("Welcome to java welcome 2  ");
+			System.out.println(" Happy Java");
+			System.out.println("i am goint to  print the loop");
+			for(int i=0;i<10;i++)
+			{
+					System.out.println("value of i="+i);
+
+			}
+
+
+
+		}
+}
